4eea7bb8d4f9378efafe4f2876cff592644ee0b2
Hey <lim2481284@gmail.com>
2017-11-15 21:16:09 +0800

    Latest design update

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,4 +20,26 @@ $(document).ready(function(){
 		$(".purchaseSection").slideUp(1000);
 		$(".attributeSection").slideDown(1000);
 	 });
+	 
+	 $('.addAttribute').click(function(){
+		$('.attributeSection').prepend('<input type="text" class="form-control" required><br>');
+	 });
+	 
+	 $('.addProductBtn').click(function(){
+		 swal({
+		  title: 'Confirm to add ?',
+		  text: "Percentage of customer that might be interested : 0.001%",
+		  type: 'info',
+		  showCancelButton: true,
+		  confirmButtonColor: '#3085d6',
+		  cancelButtonColor: '#d33',
+		  confirmButtonText: 'Yes, confirm to add!'
+		}).then(function () {
+		  swal(
+			'Cancel!',
+			'Your file has been deleted.',
+			'success'
+		  )
+		})
+	 });
 });
